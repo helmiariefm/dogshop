216da3bf1209cee3f604ec1e8e706d44c85279d6
Helmi <helmiariefm@gmail.com>
2023-06-18 23:08:49 +0700

    Home landing page ✅

---
+++ dogshop/src/components/Header.vue
@@ -0,0 +1,53 @@
+<script>
+  import { RouterLink } from 'vue-router';
+  export default{ 
+    data(){
+      return{
+        showMenu: false
+      }
+    },
+    components: { RouterLink },
+    methods: {
+      toogleMenu(){
+        this.showMenu = !this.showMenu
+      }
+    }
+  }
+</script>
+
+<template>
+  <header class="flex justify-between mx-3 md:mx-56 mt-2">
+    <RouterLink to="/">
+      <img alt="Dogshop logo" class="hover:scale-105 transition-all ease-in-out w-36" src="../assets/dogshop.png" />
+    </RouterLink>
+
+    <div id="desktop" class="my-auto hidden md:block">
+      <nav class="flex justify-between gap-5 text-primary font-bold">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/breeds">Breed</RouterLink>
+      </nav>
+    </div>
+
+    <button id="mobile" class="md:hidden" @click="toogleMenu">
+        <img class="w-7" src="http://cdn.onlinewebfonts.com/svg/img_545657.png" />
+    </button>
+
+    <div v-if="showMenu">
+      <div class="fixed top-0 left-0 w-screen h-screen z-30 bg-gray-500 bg-opacity-70 backdrop-filter backdrop-blur-sm"></div>          
+      <div class="fixed top-0 left-0 z-40 w-screen h-screen flex items-center justify-center">            
+        <div class="text-primary p-4 text-center">
+          <nav @click="toogleMenu" class='flex flex-col gap-y-9 p-9'>
+            <RouterLink to="/" class="md:text-6xl text-4xl rounded-full border-2 border-primary px-3 py-1 font-semibold">Home</RouterLink>
+            <RouterLink to="/breeds" class="md:text-6xl text-4xl rounded-full border-2 border-primary px-3 py-1 font-semibold">Breed</RouterLink>
+          </nav>
+          <button
+            class="absolute top-4 right-4 md:right-32 px-4 py-2 text-4xl hover:scale-110 transition-all ease-in-out"
+            @click="toogleMenu"
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ dogshop/src/views/Home.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<template>
+    <div class="mx-3 mt-11 md:mx-56 text-center">
+        <div class="flex flex-col-reverse md:flex-row justify-between">
+            <div class="my-auto">
+                <h1 class="text-5xl bg-clip-text text-transparent bg-gradient-to-br from-primary to-oyen md:text-7xl font-semibold text-center md:text-left h-28 md:h-40">Welcome to Dogshop</h1>            
+                <p class="text-center md:text-left">Here you can browse a catalog of dog breeds along with their respective images, based on data from "dog.ceo". <br/> Enjoy your browsing!</p>
+            </div>
+            <img alt="shib" src="../assets/shib.png" class="w-80 mx-auto md:mx-0 md:w-[584px]"/>
+        </div>
+        <button class="border-2 border-primary text-primary hover:text-secondary hover:bg-primary transition-all ease-linear px-3 py-2 text-xl rounded-full animate-bounce mt-32 md:mt-0">Let's Browse</button>
+    </div>
+</template>
